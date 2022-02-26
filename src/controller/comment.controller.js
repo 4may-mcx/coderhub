22728d9f21ec8b6a274fc3f7492617d905243db9
muchaoxu <1522928438@qq.com>
2022-02-26 15:35:09 +0800

    回复评论

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -4,8 +4,16 @@ class CommentController {
   async create(ctx, next) {
     const { id } = ctx.user;
     const { content, momentId } = ctx.request.body;
-    
+
     const result = await commentService.create(id, content, momentId);
+    ctx.body = result;
+  }
+  async reply(ctx, next) {
+    const { id } = ctx.user;
+    const { content, momentId } = ctx.request.body;
+    const { commentId } = ctx.params;
+
+    const result = await commentService.reply(id, content, momentId, commentId);
     ctx.body = result;
   }
 }
--- src/service/comment.service.js
+++ src/service/comment.service.js
@@ -7,6 +7,12 @@ class CommentService {
     const [result] = await connection.execute(statement, [content, momentId, userId]);
     return result;
   }
+
+  async reply(userId, content, momentId, commentId) {
+    const statement = `INSERT INTO comment (content, moment_id, user_id, comment_id) VALUES (?, ?, ?, ?);`;
+    const [result] = await connection.execute(statement, [content, momentId, userId, commentId]);
+    return result;
+  }
 }
 
 module.exports = new CommentService();
